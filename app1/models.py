ad398a139d700f696d31c2a2c0d301954b198de8
Carlos Leite <carlos.leite@nectosystems.com.br>
2019-10-09 21:10:25 -0300

    just comments

--- app1/models.py
+++ app1/models.py
@@ -1,5 +1,7 @@
+# import do módulo "models" do Django. 
 from django.db import models
 
+# Variável global para o Choices de tipo de lancamento
 TIPO = (
     (1, 'despesa'),
     (2, 'receita')
@@ -7,9 +9,13 @@ TIPO = (
 
 
 class Emissor(models.Model):
+    '''
+    O emissor é quem Emite a nota Fiscal
+    '''
 
     cpnj = models.CharField('CNPJ', max_length=50, primary_key=True)
     nome = models.CharField('nome', max_length=50)
+
 
     def __str__(self):
 
@@ -17,12 +23,16 @@ class Emissor(models.Model):
 
 
 class Lancamento(models.Model):
-
+    '''
+    Modelo que defini um lancamento de despesa ou receita.
+    '''
+    # ID ?? quando não especificado o Django adiciona automaticamente
+    # uma chave primária de nome "id"
     num_nota_fiscal = models.CharField('numero da nota fiscal', max_length=50, blank=True, null=True)
     receita_despesa = models.PositiveSmallIntegerField('receita ou despesa', choices=TIPO)  # True = Receita
-    #centro_custo = models.ForeignKey(CentroCusto, blank=False, null=False, on_delete=models.CASCADE)
+
     emissor = models.ForeignKey(Emissor, blank=True, null=True, on_delete=models.CASCADE)
-    # projeto = models.ForeignKey(Projeto, blank=True, null=True, on_delete=models.CASCADE)
+
     valor_nota = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     desconto = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     valor_total = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
